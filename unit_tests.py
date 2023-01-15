e77192c44e361f3e2e4fe3653ddc2b2e3da25bc1
hanzholahs <hanzholahs@gmail.com>
2023-01-15 09:40:18 +0700

    Test all code

--- unit_tests.py
+++ unit_tests.py
@@ -1,9 +1,9 @@
 import unittest
-# from test.test_genome import *
-# from test.test_creature import *
-# from test.test_motor import *
-# from test.test_simulation import *
+from test.test_genome import *
+from test.test_creature import *
+from test.test_motor import *
+from test.test_simulation import *
 from test.test_population import *
-# from test.test_manipulation import *
+from test.test_manipulation import *
 
 unittest.main()
